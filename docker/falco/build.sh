7cb4071c0f899a730ab8dc3a01b126d4531ddf9a
Nguyen Marc <nguyen_marc@live.fr>
2025-02-10 10:56:27 +0100

    chore(falco): add manual install

--- docker/falco/build.sh
+++ docker/falco/build.sh
@@ -1,4 +1,3 @@
-podman manifest rm ghcr.io/darkness4/falco:latest || true
-podman build --jobs 2 --platform linux/amd64,linux/arm64/v8 --manifest ghcr.io/darkness4/falco:latest .
-podman manifest push --all ghcr.io/darkness4/falco:latest "docker://ghcr.io/darkness4/falco:latest"
-podman manifest push --all ghcr.io/darkness4/falco:latest "docker://ghcr.io/darkness4/falco:$COMMIT_HASH"
+podman manifest rm ghcr.io/darkness4/falco:0.40.0-jemalloc-fixed || true
+podman build --jobs 2 --platform linux/amd64,linux/arm64/v8 --manifest ghcr.io/darkness4/falco:0.40.0-jemalloc-fixed .
+podman manifest push --all ghcr.io/darkness4/falco:0.40.0-jemalloc-fixed "docker://ghcr.io/darkness4/falco:0.40.0-jemalloc-fixed"
